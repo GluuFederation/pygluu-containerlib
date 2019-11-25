5038b771c133b32080070edf87a9022f7b86255d
iromli <isman.firmansyah@gmail.com>
2019-11-25 18:31:50 +0700

    add testcases for wait functions (partial)

---
+++ tests/test_wait.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+
+
+@pytest.mark.parametrize("value, expected", [
+    (10, 10),
+    (0, 1),
+    ("not_integer", 60 * 5),
+])
+def test_get_wait_max_time(value, expected):
+    from pygluu.containerlib.wait import get_wait_max_time
+
+    os.environ["GLUU_WAIT_MAX_TIME"] = str(value)
+    assert get_wait_max_time() == expected
+
+
+@pytest.mark.parametrize("value, expected", [
+    (10, 10),
+    (0, 1),
+    ("not_integer", 5),
+])
+def test_get_wait_interval(value, expected):
+    from pygluu.containerlib.wait import get_wait_interval
+
+    os.environ["GLUU_WAIT_SLEEP_DURATION"] = str(value)
+    assert get_wait_interval() == expected
