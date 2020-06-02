61e6c2dc3905f96b02aff32eca85f19d842f8ea7
iromli <isman.firmansyah@gmail.com>
2020-06-03 01:42:48 +0700

    add rclone wrapper

---
+++ pygluu/containerlib/document/rclone.py
@@ -0,0 +1,59 @@
+import logging
+import os
+
+from ..utils import exec_cmd
+
+logger = logging.getLogger(__name__)
+
+
+class RClone:
+    def __init__(self, url, username, password):
+        self.url = f"{url}/repository/default"
+        self.username = username
+        self.password = password
+
+    def configure(self):
+        conf_file = os.path.expanduser("~/.config/rclone/rclone.conf")
+        if os.path.isfile(conf_file):
+            return
+
+        cmd = f"rclone config create jackrabbit webdav vendor other pass {self.password} user admin url {self.url}"
+        _, err, code = exec_cmd(cmd)
+
+        if code != 0:
+            errors = err.decode().splitlines()
+            logger.warning(f"Unable to create webdav config; reason={errors}")
+
+    def copy_from(self, remote, local):
+        cmd = f"rclone copy jackrabbit:{remote} {local} --create-empty-src-dirs"
+        _, err, code = exec_cmd(cmd)
+
+        if code != 0:
+            errors = err.decode().splitlines()
+            logger.debug(f"Unable to sync files from remote directories; reason={errors}")
+
+    def copy_to(self, remote, local):
+        cmd = f"rclone copy {local} jackrabbit:{remote} --create-empty-src-dirs"
+        _, err, code = exec_cmd(cmd)
+
+        if code != 0:
+            errors = err.decode().splitlines()
+            logger.debug(f"Unable to sync files to remote directories; reason={errors}")
+
+    def ready(self, path="/"):
+        cmd = "rclone lsd jackrabbit:/"
+        _, err, code = exec_cmd(cmd)
+
+        if code != 0:
+            errors = err.decode().splitlines()
+            logger.debug(f"Unable to list remote directory {path}; reason={errors}")
+            return False
+        return True
+
+    def ls(self, path):
+        cmd = f"rclone ls jackrabbit:{path}"
+        out, err, code = exec_cmd(cmd)
+        if code != 0:
+            errors = err.decode().splitlines()
+            logger.debug(f"Unable to list remote directory {path}; reason={errors}")
+        return out
