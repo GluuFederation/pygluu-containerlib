ca460dc6907f21155e2bd17e2a3c3dd75d08ea41
adrian-gluu <adrian@gluu.org>
2020-06-04 02:28:28 -0300

    backport for python2

--- pygluu/containerlib/document/rclone.py
+++ pygluu/containerlib/document/rclone.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 class RClone:
     def __init__(self, url, username, password):
-        self.url = f"{url}/repository/default"
+        self.url = "{url}/repository/default"
         self.username = username
         self.password = password
 
@@ -17,28 +17,28 @@ class RClone:
         if os.path.isfile(conf_file):
             return
 
-        cmd = f"rclone config create jackrabbit webdav vendor other pass {self.password} user admin url {self.url}"
+        cmd = "rclone config create jackrabbit webdav vendor other pass {self.password} user admin url {self.url}"
         _, err, code = exec_cmd(cmd)
 
         if code != 0:
             errors = err.decode().splitlines()
-            logger.warning(f"Unable to create webdav config; reason={errors}")
+            logger.warning("Unable to create webdav config; reason={errors}")
 
     def copy_from(self, remote, local):
-        cmd = f"rclone copy jackrabbit:{remote} {local} --create-empty-src-dirs"
+        cmd = "rclone copy jackrabbit:{remote} {local} --create-empty-src-dirs"
         _, err, code = exec_cmd(cmd)
 
         if code != 0:
             errors = err.decode().splitlines()
-            logger.debug(f"Unable to sync files from remote directories; reason={errors}")
+            logger.debug("Unable to sync files from remote directories; reason={errors}")
 
     def copy_to(self, remote, local):
-        cmd = f"rclone copy {local} jackrabbit:{remote} --create-empty-src-dirs"
+        cmd = "rclone copy {local} jackrabbit:{remote} --create-empty-src-dirs"
         _, err, code = exec_cmd(cmd)
 
         if code != 0:
             errors = err.decode().splitlines()
-            logger.debug(f"Unable to sync files to remote directories; reason={errors}")
+            logger.debug("Unable to sync files to remote directories; reason={errors}")
 
     def ready(self, path="/"):
         cmd = "rclone lsd jackrabbit:/"
@@ -46,14 +46,14 @@ class RClone:
 
         if code != 0:
             errors = err.decode().splitlines()
-            logger.debug(f"Unable to list remote directory {path}; reason={errors}")
+            logger.debug("Unable to list remote directory {path}; reason={errors}")
             return False
         return True
 
     def ls(self, path):
-        cmd = f"rclone ls jackrabbit:{path}"
+        cmd = "rclone ls jackrabbit:{path}"
         out, err, code = exec_cmd(cmd)
         if code != 0:
             errors = err.decode().splitlines()
-            logger.debug(f"Unable to list remote directory {path}; reason={errors}")
+            logger.debug("Unable to list remote directory {path}; reason={errors}")
         return out
