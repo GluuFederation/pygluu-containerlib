93a200c3558daa012bca2d3bb7e789bf4f367c86
iromli <isman.firmansyah@gmail.com>
2021-01-28 02:19:39 +0700

    feat(couchbase): switch ports used by REST and N1QL clients based on truststore.enable toggle

--- pygluu/containerlib/persistence/couchbase.py
+++ pygluu/containerlib/persistence/couchbase.py
@@ -291,6 +291,14 @@ class BaseClient:
         self._session = None
 
     @property
+    def scheme(self):
+        """Scheme used when connecting to Couchbase server.
+        """
+        if as_boolean(os.environ.get("GLUU_COUCHBASE_TRUSTSTORE_ENABLE", True)):
+            return "https"
+        return "http"
+
+    @property
     def session(self):
         """Get an instance of ``requests.Session``.
 
@@ -353,8 +361,13 @@ class N1qlClient(BaseClient):
     """This class interacts with N1QL server (part of Couchbase).
     """
 
-    #: Port where N1QL server is bind.
-    port = 18093
+    @property
+    def port(self):
+        """Port where N1QL server is bind to.
+        """
+        if as_boolean(os.environ.get("GLUU_COUCHBASE_TRUSTSTORE_ENABLE", True)):
+            return 18093
+        return 8093
 
     def healthcheck(self, host):
         """Run healthcheck to a host.
@@ -363,7 +376,7 @@ class N1qlClient(BaseClient):
         :returns: An instance of ``requests.models.Response``.
         """
         return self.session.post(
-            f"https://{host}:{self.port}/query/service",
+            f"{self.scheme}://{host}:{self.port}/query/service",
             data={"statement": "SELECT status FROM system:indexes LIMIT 1"},
             auth=(self.user, self.password),
             timeout=10,
@@ -379,7 +392,7 @@ class N1qlClient(BaseClient):
         data = kwargs.get("data", {})
 
         resp = self.session.post(
-            f"https://{self.host}:{self.port}/{path}",
+            f"{self.scheme}://{self.host}:{self.port}/{path}",
             data=data,
             auth=(self.user, self.password),
         )
@@ -415,8 +428,13 @@ class RestClient(BaseClient):
     """This class interacts with REST server (part of Couchbase).
     """
 
-    #: Port where REST server is bind.
-    port = 18091
+    @property
+    def port(self):
+        """Port where REST server is bind to.
+        """
+        if as_boolean(os.environ.get("GLUU_COUCHBASE_TRUSTSTORE_ENABLE", True)):
+            return 18091
+        return 8091
 
     def healthcheck(self, host):
         """Run healthcheck to a host.
@@ -425,7 +443,7 @@ class RestClient(BaseClient):
         :returns: An instance of ``requests.models.Response``.
         """
         return self.session.get(
-            f"https://{host}:{self.port}/pools/",
+            f"{self.scheme}://{host}:{self.port}/pools/",
             auth=(self.user, self.password),
             timeout=10,
         )
@@ -451,7 +469,7 @@ class RestClient(BaseClient):
             raise ValueError(f"Unsupported method {method}")
 
         resp = req(
-            f"https://{self.host}:{self.port}/{path}",
+            f"{self.scheme}://{self.host}:{self.port}/{path}",
             auth=(self.user, self.password),
         )
         return resp
