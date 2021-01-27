8a6911a8b104c1b3604977e769e3c8eae53d8dcc
iromli <isman.firmansyah@gmail.com>
2021-01-28 00:10:25 +0700

    feat(unencrypted-couchbase): add environment variable to disable truststore for couchbase (close #22)

--- pygluu/containerlib/persistence/couchbase.py
+++ pygluu/containerlib/persistence/couchbase.py
@@ -243,7 +243,9 @@ def render_couchbase_properties(manager, src: str, dest: str) -> None:
                 "default_bucket": bucket_prefix,
                 "couchbase_mappings": "\n".join(couchbase_mappings),
                 "encryption_method": "SSHA-256",
-                "ssl_enabled": "true",
+                "ssl_enabled": str(as_boolean(
+                    os.environ.get("GLUU_COUCHBASE_TRUSTSTORE_ENABLE", True)
+                )).lower(),
                 "couchbaseTrustStoreFn": manager.config.get("couchbaseTrustStoreFn"),
                 "encoded_couchbaseTrustStorePass": encode_text(
                     GLUU_COUCHBASE_TRUSTSTORE_PASSWORD,
